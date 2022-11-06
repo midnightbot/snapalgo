28afbd387be6360230be18a2da270a14a88fe1a4
Anish Adnani <44056065+midnightbot@users.noreply.github.com>
2022-11-05 19:03:23 -0700

    0.0.3.1

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 from setuptools import setup, find_packages
  
- 
 setup(
   name='snapalgo',
-  version='0.0.3',
+  version='0.0.3.1',
   description='Various Coding algorithms',
   long_description='Algorithm Support',
-  url='',  
+  url='https://github.com/midnightbot/snapalgo',  
   author='Anish Adnani',
   author_email='anishadnani00@gmail.com',
   license='MIT', 
@@ -14,7 +13,7 @@ setup(
   packages=find_packages(),
   install_requires=[''] ,
   classifiers=[
-        "Development Status :: 1 - Planning",
+        "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
         "Operating System :: Unix",
